e66ab3d2625996186783038e5aa661553d0c8d71
Virx <virx@virxcase.dev>
2025-01-17 20:35:19 -0500

    Add initial pack algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
-use anyhow::anyhow;
 use clap::{Parser, Subcommand};
 
 mod build;
 mod buildinfo;
 mod config;
+mod pack;
 
 #[derive(Parser)]
 struct Cli {
@@ -21,7 +21,7 @@ enum Command {
     Build(BuildCommand),
 
     /// Package a bob output dir
-    Pack { dir: PathBuf },
+    Pack(PathCommand),
 }
 
 #[derive(Parser, Debug)]
@@ -32,22 +32,19 @@ struct BuildCommand {
     out_dir: PathBuf,
 }
 
+#[derive(Parser, Debug)]
+struct PathCommand {
+    #[arg(default_value = "./bob_build")]
+    build_dir: PathBuf,
+    #[arg(short, long)]
+    old_buildinfo: Option<PathBuf>,
+}
+
 fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let cli = Cli::parse();
     match cli.command {
         Command::Build(x) => build::build(x),
-        Command::Pack { dir } => pack(dir),
+        Command::Pack(x) => pack::pack(x),
     }
-}
-
-fn pack(dir: PathBuf) -> anyhow::Result<()> {
-    if !fs::exists(&dir)? {
-        return Err(anyhow!("Directory doesn't exist"));
-    }
-
-    // this is probably gonna be a built in .tar.xz archiver or something similar
-    todo!("packing/unpacking");
-
-    Ok(())
 }
---
+++ src/pack.rs
@@ -0,0 +1,116 @@
+use std::fs::File;
+use std::io::copy;
+use std::path::Path;
+use std::{fs, time::Instant};
+
+use crate::{
+    buildinfo::{BuildInfo, Project},
+    PathCommand,
+};
+use anyhow::anyhow;
+use liblzma::read::XzEncoder;
+use tar::Builder;
+
+pub fn pack(command: PathCommand) -> anyhow::Result<()> {
+    if !fs::exists(&command.build_dir)? {
+        return Err(anyhow!("Directory doesn't exist"));
+    }
+
+    let buildinfo_path = command.build_dir.join("buildinfo.toml");
+    if !fs::exists(&buildinfo_path)? {
+        return Err(anyhow!("buildinfo.toml doesn't exist"));
+    }
+
+    let str_contents = fs::read_to_string(&buildinfo_path)?;
+    let mut buildinfo: BuildInfo = toml::from_str(&str_contents)?;
+
+    create_archives(&buildinfo.projects, &command.build_dir, "full")?;
+
+    if let Some(old_buildinfo) = &command.old_buildinfo {
+        if !fs::exists(old_buildinfo)? {
+            return Err(anyhow!("old_buildinfo doesn't exist"));
+        }
+        let str_contents = fs::read_to_string(old_buildinfo)?;
+        let old_buildinfo: BuildInfo = toml::from_str(&str_contents)?;
+
+        // remove projects that haven't changed
+        for old_project in old_buildinfo.projects {
+            let project_idx = buildinfo
+                .projects
+                .iter()
+                .position(|p| p.name == old_project.name);
+            if let Some(idx) = project_idx {
+                if buildinfo.projects[idx].hash == old_project.hash {
+                    buildinfo.projects.remove(idx);
+                }
+            }
+        }
+    }
+
+    create_archives(&buildinfo.projects, &command.build_dir, "diff")?;
+
+    Ok(())
+}
+
+fn create_archives(projects: &[Project], build_dir: &Path, name: &str) -> anyhow::Result<()> {
+    let compression_start = Instant::now();
+
+    let platforms = ["x86_64-pc-windows-msvc", "x86_64-unknown-linux-gnu"];
+
+    for platform in platforms {
+        let archive_name = format!("{platform}-{name}.tar.xz");
+        println!("Now creating archive {archive_name}");
+
+        let mut a = Builder::new(Vec::new());
+
+        let mut buildinfo = File::open(build_dir.join("buildinfo.toml"))?;
+        a.append_file("buildinfo.toml", &mut buildinfo).unwrap();
+
+        for project in projects {
+            let project_dir = build_dir.join(&project.name);
+            if !project_dir.exists() {
+                return Err(anyhow!(
+                    "Project directory for {} no longer exists",
+                    project.name
+                ));
+            }
+
+            let platform_dir = project_dir.join(platform);
+            if !platform_dir.exists() {
+                continue;
+            }
+
+            a.append_dir_all(format!("{}/{platform}", project.name), platform_dir)
+                .unwrap();
+
+            for entry in fs::read_dir(&project_dir)?.into_iter().flatten() {
+                let path = entry.path();
+                if path.is_dir() {
+                    continue;
+                }
+
+                let archive_file_name = format!(
+                    "{}/{}",
+                    project.name,
+                    path.file_name().unwrap().to_str().unwrap()
+                );
+
+                let mut file = File::open(&path)?;
+                a.append_file(archive_file_name, &mut file).unwrap();
+            }
+        }
+
+        let raw_data = a.into_inner().unwrap();
+        let mut compressed_data = XzEncoder::new(raw_data.as_slice(), 9);
+
+        let mut file = File::create(build_dir.join(archive_name))?;
+        copy(&mut compressed_data, &mut file)?;
+    }
+
+    println!(
+        "Created {name} archives in {:2}s",
+        compression_start.elapsed().as_secs_f32()
+    );
+
+    Ok(())
+}
